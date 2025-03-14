4ca54cf64d8551dccfa9f4fe2efde47613f0b68a
manu <manuelugoccioni@gmail.com>
2025-03-14 20:16:30 +0100

    tabs for gdscript files

--- lua/options.lua
+++ lua/options.lua
@@ -6,3 +6,10 @@ o.wrap = false
 o.relativenumber = true
 o.shiftwidth = 4
 o.tabstop = 4
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "gdscript",
+    callback = function()
+        vim.opt_local.expandtab = false
+    end,
+})
